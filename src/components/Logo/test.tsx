a61ee3839a53700549531a8f5418f3aa39976085
Gustavo Granato <gustavo.granato@hotmail.com>
2022-04-30 18:50:15 -0300

    test: create tests for logo components

--- src/components/Logo/test.tsx
+++ src/components/Logo/test.tsx
@@ -1,5 +1,6 @@
 import { screen } from '@testing-library/react'
-import { renderWithTheme } from 'utils/helpers/tests'
+import { renderWithTheme } from 'utils/tests/helper'
+import 'jest-styled-components'
 
 import Logo from '.'
 
@@ -13,8 +14,33 @@ describe('<Logo />', () => {
 
   it('should render a black label when color is passed', () => {
     renderWithTheme(<Logo color="black" />)
-    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
+    expect(screen.getByLabelText(/^won games$/i).parentElement).toHaveStyle({
       color: '#030517'
     })
+  })
+
+  it('should render a bigger logo', () => {
+    renderWithTheme(<Logo color="black" size="large" />)
+    expect(screen.getByRole('img').parentElement).toHaveStyle({
+      width: '20rem'
+    })
+  })
+
+  it('should render a normal logo by default ', () => {
+    renderWithTheme(<Logo color="black" />)
+    expect(screen.getByRole('img').parentElement).toHaveStyle({
+      width: '11rem'
+    })
+  })
+
+  it(`should render logo without text when hideOnMobile is passed and it's less than medium(768px)`, () => {
+    renderWithTheme(<Logo hideOnMobile />)
+    expect(screen.getByRole('img').parentElement).toHaveStyleRule(
+      'width',
+      '5.8rem',
+      {
+        media: '(max-width:768px)'
+      }
+    )
   })
 })
